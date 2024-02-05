b3a4ce886a7c0380b38b2d7f8348ce0fffbb79a4
Killklli <tvatter@hotmail.com>
2024-02-05 16:42:34 +0000

    Automated release: 2024-02-05, 16:42:34

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 """Holds the version for DK64 Rando."""
 
-version = "3.0.3"
+version = "3.0.7"
 split_data = version.split(".")
 major = split_data[0]
 minor = split_data[1]
 patch = split_data[2]
 
-whl_hash = "4f19fb8eac29b4b2595eb765eff5b958"
+whl_hash = "2eb235075946fb975bade4db9e1b2178"

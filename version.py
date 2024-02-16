763906a1c1f525385f7ab255755f4c24414d2c8a
pbeierle22 <pbeierle22@gmail.com>
2024-02-16 14:34:31 +0000

    Automated release: 2024-02-16, 14:34:31

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 """Holds the version for DK64 Rando."""
 
-version = "3.0.17"
+version = "3.0.22"
 split_data = version.split(".")
 major = split_data[0]
 minor = split_data[1]
 patch = split_data[2]
 
-whl_hash = "e3a20428e02a1711abb9737be485f93e"
+whl_hash = "682955915d66ea97e618d7d2bb707493"

cc254c227f2ecac8bd6571b9e5c6c73a9afb1d4f
theballaam96 <theballaam96@gmail.com>
2024-05-07 14:34:28 +0000

    Automated release: 2024-05-07, 14:34:28

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 """Holds the version for DK64 Rando."""
 
-version = "3.0.49"
+version = "3.1.0"
 split_data = version.split(".")
 major = split_data[0]
 minor = split_data[1]
 patch = split_data[2]
 
-whl_hash = "33fdd03f7666ba8e87e85393dcefa06a"
+whl_hash = "a363afa4607b2e8dcbeb7d8f43c613d7"
